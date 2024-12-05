60bf6156ddb2e60849b5bd549a919893cb480d74
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-05 14:48:14 +0100

    Added recipesReducer and installed redux and redux thunk

---
+++ culinary-app/src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { recipesReducer } from './reducers/recipesReducer';
+
+
+const rootReducer = combineReducers({
+  recipes: recipesReducer,
+});
+
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
