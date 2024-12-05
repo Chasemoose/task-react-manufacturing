b733de662ccb2079af567486b76d6943b8c8d4f9
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-05 15:58:20 +0100

    changed App.js file

--- culinary-app/src/App.js
+++ culinary-app/src/App.js
@@ -1,37 +1,15 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { addRecipe, removeRecipe, toggleFavorite } from './redux/actions/recipeActions'
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipe from './components/AddRecipe';
 import './App.css';
 
 function App() {
-  const recipes = useSelector(state => state.recipes.recipes)
-  const dispatch = useDispatch()
-
-const handleAddRecipe = () => {
-  const newRecipe = {id: Date.now(), name: 'Nowy przepis', isFavorite: false }
-  dispatch(addRecipe(newRecipe))
-}
-
-
-
-
   return (
     <div>
-    <h1>Recipe Manager</h1>
-    <button onClick={handleAddRecipe}>Add Recipe</button>
-    <ul>
-      {recipes.map(recipe => (
-        <li key={recipe.id}>
-          {recipe.name}{' '}
-          <button onClick={() => dispatch(toggleFavorite(recipe.id))}>
-            {recipe.isFavorite ? 'Unfavorite' : 'Favorite'}
-          </button>
-          <button onClick={() => dispatch(removeRecipe(recipe.id))}>Delete</button>
-        </li>
-      ))}
-    </ul>
-  </div>
-
+      <h1>Recipe Manager</h1>
+      <AddRecipe />
+      <RecipeList />
+    </div>
   );
 }
 
