0c31498ad6a4a6009dfdde22327171dbde365549
Jaro <jaroswidnik@interia.pl>
2024-12-06 13:47:35 +0100

    Modified RecipeList.js file

--- culinary-app/src/components/RecipeList.js
+++ culinary-app/src/components/RecipeList.js
@@ -1,33 +1,24 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { toggleFavorite, removeRecipe } from '../redux/actions/recipeActions'
+import React from "react";
+import { useSelector } from "react-redux";
+import RecipeItem from "./RecipeItem";
 
 const RecipeList = () => {
-    const recipes = useSelector (state => state.recipes.recipes)
-    const dispatch = useDispatch()
+	const recipes = useSelector(state => state.recipes.recipes);
 
-    if (recipes.length === 0) {
-        return <p>Brak dostępnych przepisów. Dodaj jakieś!</p>
-    }
+	if (recipes.length === 0) {
+		return <p>Brak dostępnych przepisów. Dodaj jakiś!</p>;
+	}
 
-    return (
-        <div>
-        <h2>Recipes</h2>
-        <ul>
-          {recipes.map(recipe => (
-            <li key={recipe.id}>
-              <span>{recipe.name}</span>
-              <button onClick={() => dispatch(toggleFavorite(recipe.id))}>
-                {recipe.isFavorite ? 'Unfavorite' : 'Favorite'}
-              </button>
-              <button onClick={() => dispatch(removeRecipe(recipe.id))}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div> 
-    )
+	return (
+		<div>
+			<h2>Przepisy</h2>
+			<ul>
+				{recipes.map(recipe => (
+					<RecipeItem key={recipe.id} recipe={recipe} />
+				))}
+			</ul>
+		</div>
+	);
+};
 
-
-}
-
-export default RecipeList
+export default RecipeList;
