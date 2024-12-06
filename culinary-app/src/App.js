95b9fdcf5b696d7b61a804a3c6572bc3ff05027b
Jaro <jaroswidnik@interia.pl>
2024-12-06 15:48:32 +0100

    Modified App.js with AddRecipe and data files

--- culinary-app/src/App.js
+++ culinary-app/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipe from './components/AddRecipe';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setRecipes } from "./redux/actions/recipeActions";
+import RecipeList from "./components/RecipeList";
+import AddRecipe from "./components/AddRecipe";
+import "./App.css";
+import data from "./data/recipes.json";
 
 function App() {
-  return (
-    <div>
-      <h1>Recipe Manager</h1>
-      <AddRecipe />
-      <RecipeList />
-    </div>
-  );
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(setRecipes(data));
+	}, [dispatch]);
+
+	return (
+		<div>
+			<h1>Przepisbook</h1>
+			<AddRecipe />
+			<RecipeList />
+		</div>
+	);
 }
 
 export default App;
