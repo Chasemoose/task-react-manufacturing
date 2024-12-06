db18fc255a2f934d09f3b58c5dc8f85cd8465937
Jaro <jaroswidnik@interia.pl>
2024-12-06 16:16:12 +0100

    Added update recipe case in reducer

--- culinary-app/src/redux/reducers/recipesReducer.js
+++ culinary-app/src/redux/reducers/recipesReducer.js
@@ -1,30 +1,40 @@
 const initialState = {
-    recipes: [],
-    favorites: [],
-}
+	recipes: [],
+	favorites: [],
+};
 
 export const recipesReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'ADD_RECIPE':
-            return {
-                ...state,
-                recipes: [...state.recipes, action.payload]
-            }
-        case 'REMOVE_RECIPE':
-            return {
-                ...state,
-                recipes: state.recipes.filter(recipe => recipe.id !== action.payload)
-            }
-        case 'TOGGLE_FAVORITE':
-                const updateRecipes = state.recipes.map(recipe => recipe.id === action.payload ? { ...recipe, isFavorite: !recipe.isFavorite} : recipe
-                )
-                return {
-                    ...state, 
-                    recipes: updateRecipes,
-                    favorites: updateRecipes.filter(recipe => recipe.isFavorite),
-                }           
-            default: 
-                return state        
+	switch (action.type) {
+		case "ADD_RECIPE":
+			return {
+				...state,
+				recipes: [...state.recipes, action.payload],
+			};
+		case "REMOVE_RECIPE":
+			return {
+				...state,
+				recipes: state.recipes.filter(recipe => recipe.id !== action.payload),
+			};
+		case "TOGGLE_FAVORITE":
+			const updateRecipes = state.recipes.map(recipe =>
+				recipe.id === action.payload
+					? { ...recipe, isFavorite: !recipe.isFavorite }
+					: recipe
+			);
+			return {
+				...state,
+				recipes: updateRecipes,
+				favorites: updateRecipes.filter(recipe => recipe.isFavorite),
+			};
 
-    }
-}
+		case "UPDATE_RECIPE":
+			return {
+				...state,
+				recipes: state.recipes.map(recipe =>
+					recipe.id === action.payload.id ? action.payload : recipe
+				),
+			};
+		default:
+			return state;
+	}
+};
