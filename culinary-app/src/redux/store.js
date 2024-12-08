a02161038a874e4fdf879adc475c4723d7678071
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-08 12:58:21 +0100

    Changed store.js

--- culinary-app/src/redux/store.js
+++ culinary-app/src/redux/store.js
@@ -2,10 +2,23 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 import { recipesReducer } from './reducers/recipesReducer';
 
+const persistedFavorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+const initialState = {
+    recipes: {
+        recipes: [],
+        favorites: persistedFavorites,  
+    },
+};
 
 const rootReducer = combineReducers({
   recipes: recipesReducer,
 });
 
+const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+);
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export default store;
