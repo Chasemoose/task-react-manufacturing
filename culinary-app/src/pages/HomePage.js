c2600872181c3df9c9eedb71bec92a7f078ad98c
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-08 20:10:38 +0100

    Changes in css background

--- culinary-app/src/pages/HomePage.js
+++ culinary-app/src/pages/HomePage.js
@@ -1,6 +1,12 @@
+import React from "react";
+import '../styles/HomePage.css'
+
 const HomePage = () => {
-    return <h2>Welcome to Przepisbook!</h2>;
-  };
-  
-  export default HomePage;
-  
+    return (
+        <div className="homepage-container">
+            <h2 className="homepage-header">Welcome to Przepisbook!</h2>
+        </div>
+    );
+};
+
+export default HomePage;
