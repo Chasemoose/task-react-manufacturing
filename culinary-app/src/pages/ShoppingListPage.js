bbfcec360bbf7f630fac76e4309f5f5d42cd4c45
unknown <kerajas86@gmail.com>
2024-12-11 17:17:07 +0100

    Added more styles to shopping list files

--- culinary-app/src/pages/ShoppingListPage.js
+++ culinary-app/src/pages/ShoppingListPage.js
@@ -6,6 +6,7 @@ const ShoppingListPage = () => {
   return (
     <div className="shopping-list-page-container">
       <div className="overlay"></div>
+      <h2 className="shoppingList-page-title">Lista zakupów</h2>
       <ShoppingList />
     </div>
   );
