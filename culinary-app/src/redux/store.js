0ce6d793e3469bbab925db7e4d7cb9519e7136db
unknown <kerajas86@gmail.com>
2024-12-11 17:57:48 +0100

    Added changes in store.js

--- culinary-app/src/redux/store.js
+++ culinary-app/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { thunk } from 'redux-thunk';
+import { thunk } from 'redux-thunk'; 
 import { recipesReducer } from './reducers/recipesReducer';
 
 const persistedFavorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
@@ -8,6 +8,8 @@ const initialState = {
     recipes: {
         recipes: [],
         favorites: persistedFavorites,  
+        searchHistory: [],  
+        shoppingList: [],   
     },
 };
 
