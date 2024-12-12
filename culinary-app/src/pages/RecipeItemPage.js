ca873e3f9e891691473e4983b45c57fa36ed0c76
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-12 17:42:50 +0100

    Styled recipeItemPage.js

--- culinary-app/src/pages/RecipeItemPage.js
+++ culinary-app/src/pages/RecipeItemPage.js
@@ -1,42 +1,12 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
-// import '../styles/RecipeItemPage.css'
-
-// const RecipeItemPage = () => {
-//   const { id } = useParams();
-//   const recipes = useSelector((state) => state.recipes.recipes);
-//   const recipe = recipes.find((r) => r.id === parseInt(id));
-
-//   if (!recipe) {
-//     return <p className="recipe-item-page-not-found">Przepis nie został znaleziony.</p>;
-//   }
-
-//   return (
-//     <div className="recipe-item-page-container">
-//       <h2 className="recipe-item-page-title">{recipe.name}</h2>
-//       <p className="recipe-item-page-ingredients">
-//         <strong>Składniki:</strong> {recipe.ingredients.join(", ")}
-//       </p>
-//       <p className="recipe-item-page-instructions">
-//         <strong>Instrukcje:</strong> {recipe.instructions}
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default RecipeItemPage;
-
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { toggleFavorite } from "../redux/actions/recipeActions"; // Importuj akcję
+import { toggleFavorite } from "../redux/actions/recipeActions";
 import '../styles/RecipeItemPage.css';
 
 const RecipeItemPage = () => {
   const { id } = useParams();
-  const dispatch = useDispatch(); // Użyj dispatch do wywołania akcji
+  const dispatch = useDispatch();
   const recipes = useSelector((state) => state.recipes.recipes);
   const recipe = recipes.find((r) => r.id === parseInt(id));
 
@@ -46,7 +16,7 @@ const RecipeItemPage = () => {
 
   const handleToggleFavorite = () => {
     if (recipe) {
-      dispatch(toggleFavorite(recipe.id)); // Wywołaj akcję toggleFavorite
+      dispatch(toggleFavorite(recipe.id));
     }
   };
 
@@ -56,22 +26,24 @@ const RecipeItemPage = () => {
 
   return (
     <div className="recipe-item-page-container">
-      <h2 className="recipe-item-page-title">{recipe.name}</h2>
-      <p className="recipe-item-page-ingredients">
-        <strong>Składniki:</strong> {recipe.ingredients.join(", ")}
-      </p>
-      <p className="recipe-item-page-instructions">
-        <strong>Instrukcje:</strong> {recipe.instructions}
-      </p>
-
-      <button
-        className={`recipe-item-page-favorite-button ${
-          isFavorite ? "remove" : "add"
-        }`}
-        onClick={handleToggleFavorite}
-      >
-        {isFavorite ? "Usuń z ulubionych" : "Dodaj do ulubionych"}
-      </button>
+      <div className="recipe-item-page-overlay"></div>
+      <div className="recipe-item-content">
+        <h2 className="recipe-item-page-title">{recipe.name}</h2>
+        <p className="recipe-item-page-ingredients">
+          <strong>Składniki:</strong> {recipe.ingredients.join(", ")}
+        </p>
+        <p className="recipe-item-page-instructions">
+          <strong>Instrukcje:</strong> {recipe.instructions}
+        </p>
+        <button
+          className={`recipe-item-page-favorite-button ${
+            isFavorite ? "remove" : "add"
+          }`}
+          onClick={handleToggleFavorite}
+        >
+          {isFavorite ? "Usuń z ulubionych" : "Dodaj do ulubionych"}
+        </button>
+      </div>
     </div>
   );
 };
