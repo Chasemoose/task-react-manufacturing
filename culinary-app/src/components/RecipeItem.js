b7d96dc10302a9c9cdf9f9964910e68646c4e324
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-12 18:04:36 +0100

    Added styles to RecipeItem.js

--- culinary-app/src/components/RecipeItem.js
+++ culinary-app/src/components/RecipeItem.js
@@ -2,40 +2,52 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { toggleFavorite, removeRecipe } from "../redux/actions/recipeActions";
 import EditRecipe from "./EditRecipe";
+import '../styles/RecipeItem.css'; // Importujemy plik stylów
 
 const RecipeItem = ({ recipe }) => {
-	const [isEditing, setIsEditing] = useState(false);
-	const dispatch = useDispatch();
+  const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useDispatch();
 
-	return (
-		<li>
-			{isEditing ? (
-				<EditRecipe recipe={recipe} onClose={() => setIsEditing(false)} />
-			) : (
-				<>
-					<h3>{recipe.name}</h3>
-					<p>
-						<strong>Ingredients:</strong>
-					</p>
-					<ul>
-						{recipe.ingredients.map((ingredient, index) => (
-							<li key={index}>{ingredient}</li>
-						))}
-					</ul>
-					<p>
-						<strong>Instructions:</strong> {recipe.instructions}
-					</p>
-					<button onClick={() => dispatch(toggleFavorite(recipe.id))}>
-						{recipe.isFavorite ? "Unfavorite" : "Favorite"}
-					</button>
-					<button onClick={() => setIsEditing(true)}>Edit</button>
-					<button onClick={() => dispatch(removeRecipe(recipe.id))}>
-						Delete
-					</button>
-				</>
-			)}
-		</li>
-	);
+  return (
+    <li className="recipe-item-container">
+      {isEditing ? (
+        <EditRecipe recipe={recipe} onClose={() => setIsEditing(false)} />
+      ) : (
+        <>
+          <h3 className="recipe-item-title">{recipe.name}</h3>
+          <p className="recipe-item-ingredients-header">
+            <strong>Składniki:</strong>
+          </p>
+          <ul className="recipe-item-ingredients-list">
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index} className="recipe-item-ingredient">{ingredient}</li>
+            ))}
+          </ul>
+          <p className="recipe-item-instructions">
+            <strong>Sposób przygotowania:</strong> {recipe.instructions}
+          </p>
+          <button
+            className={`recipe-item-button ${recipe.isFavorite ? "remove" : "add"}`}
+            onClick={() => dispatch(toggleFavorite(recipe.id))}
+          >
+            {recipe.isFavorite ? "Nieulubiony" : "Ulubiony"}
+          </button>
+          <button
+            className="recipe-item-button edit"
+            onClick={() => setIsEditing(true)}
+          >
+            Edytuj
+          </button>
+          <button
+            className="recipe-item-button remove"
+            onClick={() => dispatch(removeRecipe(recipe.id))}
+          >
+            Usuń
+          </button>
+        </>
+      )}
+    </li>
+  );
 };
 
 export default RecipeItem;
