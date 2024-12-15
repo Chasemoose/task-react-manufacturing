9188b97434faa93bc0cc051b66fce34fb44c0403
Jarek Stelmach <j.stelmach@onet.eu>
2024-12-15 17:04:57 +0100

    Added footer and backlink

--- culinary-app/src/App.js
+++ culinary-app/src/App.js
@@ -1,53 +1,58 @@
 import React from "react";
-// import { useDispatch } from "react-redux";
-// import { fetchRecipes } from "./redux/actions/recipeActions";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// import AddRecipePage from "./pages/AddRecipePage";
 import HomePage from "./pages/HomePage";
 import SearchRecipesPage from "./pages/SearchRecipesPage";
-import RecipeItemPage from "./pages/RecipeItemPage"; 
+import RecipeItemPage from "./pages/RecipeItemPage";
 import ErrorBoundary from "./ErrorBoundary";
 import FavoriteRecipesPage from "./pages/FavoriteRecipesPage";
-import ShoppingListPage from "./pages/ShoppingListPage"; 
+import ShoppingListPage from "./pages/ShoppingListPage";
 import "./styles/App.css";
 
 function App() {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(fetchRecipes()); 
-  // }, [dispatch]);
-
   return (
     <Router>
       <ErrorBoundary>
-        <nav className="nav-bar">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            {/* <li>
-              <Link to="/add">Dodaj przepis</Link>
-            </li> */}
-            <li>
-              <Link to="/search">Recipe search</Link>
-            </li>
-            <li>
-              <Link to="/favorites">Favorite recipes</Link>
-            </li>
-            <li>
-              <Link to="/shopping-list">Shopping list</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* <Route path="/add" element={<AddRecipePage />} /> */}
-          <Route path="/search" element={<SearchRecipesPage />} />
-          <Route path="/recipe/:id" element={<RecipeItemPage />} />
-          <Route path="/favorites" element={<FavoriteRecipesPage />} />
-          <Route path="/shopping-list" element={<ShoppingListPage />} />
-        </Routes>
+        <div className="app-container">
+          <nav className="nav-bar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search">Recipe search</Link>
+              </li>
+              <li>
+                <Link to="/favorites">Favorite recipes</Link>
+              </li>
+              <li>
+                <Link to="/shopping-list">Shopping list</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/search" element={<SearchRecipesPage />} />
+              <Route path="/recipe/:id" element={<RecipeItemPage />} />
+              <Route path="/favorites" element={<FavoriteRecipesPage />} />
+              <Route path="/shopping-list" element={<ShoppingListPage />} />
+            </Routes>
+          </main>
+
+          <footer className="app-footer">
+            <p>
+              Powered by{" "}
+              <a
+                href="https://spoonacular.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Spoonacular API
+              </a>
+            </p>
+          </footer>
+        </div>
       </ErrorBoundary>
     </Router>
   );
